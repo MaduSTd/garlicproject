d5273653cb487d0455a04a762b401925090fb99d
madustd <m.hanysakran@gmail.com>
2023-10-15 01:29:01 +0300

    8th

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,8 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
