ed32bd10b9da5df2a156a606f6afd7171df74837
madustd <m.hanysakran@gmail.com>
2023-10-26 16:14:20 +0300

    16th

--- resources/views/website/layouts/navbar.blade.php
+++ resources/views/website/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-light shadow">
         <div class="container d-flex justify-content-between align-items-center">
 
-            <a class="navbar-brand text-success logo h1 align-self-center" href="index.html">
+            <a class="h2 text-success border-bottom pb-3 border-light logo" href="index.html">
                 Mazarea El-Kaliubia
             </a>
 
