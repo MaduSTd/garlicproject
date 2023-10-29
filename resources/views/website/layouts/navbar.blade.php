2e5a07ad3adf02190e0cbee547e2b47945307c15
madustd <m.hanysakran@gmail.com>
2023-10-29 20:47:46 +0200

    update error navbar

--- resources/views/website/layouts/navbar.blade.php
+++ resources/views/website/layouts/navbar.blade.php
@@ -6,7 +6,6 @@
         <li><a href="{{route('website.home')}}" >Home</a></li>
         <li><a href="{{route('website.about')}}">About Us</a></li>
         <li><a href="{{route('website.contact')}}">Contact Us</a></li>
-<<<<<<< HEAD
         <li >
             <a href="#">
              <select name="languages">
@@ -17,16 +16,6 @@
             </select></a>
 
         </li>
-=======
-        <li><a href="#">Contact</a>
-            <ul>
-              <li><a href="#">Map</a></li>
-              <li><a href="#">Form</a></li>
-            </ul>
-          </li>
-        </ul>
-
->>>>>>> 537227d5460886c9825d6f5787bf6ef220d285b4
       </ul>
     </div>
   </div>
